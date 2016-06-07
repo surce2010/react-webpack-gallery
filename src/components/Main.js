03cda173625869ee190b0d1b675db6942fd11429
surce2010 <345561171@qq.com>
2016-06-07 14:19:37 +0800

    TODO 重新布局图片

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,6 @@
 require('normalize.css/normalize.css');
 require('styles/App.css');
+require('styles/main.less');
 
 import React from 'react';
 import _ from 'lodash';
@@ -18,18 +19,15 @@ imagesData = ((imagesDataArr) => {
 })(imagesData);
 
 class ImageFigure extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
-  	let me = this;
-  	let { props } = me;
+  	let me = this,
+        props = me.props,
+        pos = _.get(props, 'pos');
 
     return (
-    	<figure>
-        <figcaption>{_.get(props, 'imagesData.fileName')}</figcaption>
+    	<figure className="img-figure" ref={(c) => {this.figure = c}} style={pos}>
         <img src={_.get(props, 'imagesData.imageURL')} />
+        <figcaption className="img-title">{_.get(props, 'imagesData.fileName')}</figcaption>
       </figure>
     );
   }
@@ -38,20 +36,98 @@ class ImageFigure extends React.Component {
 class AppComponent extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      centerPos: { //中间取值范围
+        left: 0,
+        top: 0
+      },
+      hPosRange: { //水平方向取值范围
+        leftSecX: [0, 0],
+        rightSecX: [0, 0],
+        y: [0, 0]
+      },
+      vPosRange: { //垂直方向取值范围
+        x: [0, 0],
+        topY: [0, 0]
+      },
+      imagesPos: [] //图片数组位置
+    };
   }
 
   render() {
+    let me = this,
+        state = me.state,
+        imgFiguresJSX = [],
+        controllerUnits = [];
 
-  	let imgFiguresJSX = [];
-  	_.map(imagesData, (item) => {
-  		imgFiguresJSX.push(<ImageFigure imagesData={item} />);
-  	});
+  	_.map(imagesData, function (item, index) {
+      if (!_.get(state, ['imagesPos', index])) {
+        _.set(state, ['imagesPos', index], {left:0, top:0});
+      }
+
+  		imgFiguresJSX.push(
+        <ImageFigure key={index} imagesData={item} ref={'imgFigure' + index} pos={_.get(state, ['imagesPos', index])} />
+      );
+  	}.bind(me));
 
     return (
-      <div className="index">
-      	{imgFiguresJSX}
-      </div>
+      <section className="stage" ref="stage">
+        <section className="img-sec">
+          {imgFiguresJSX}
+        </section>
+        <nav className="controller-nav">
+          {controllerUnits}
+        </nav>
+      </section>
     );
+  }
+
+  //组件加载以后,为每一个图片计算位置取值区间
+  componentDidMount() {
+    let me = this,
+        state = me.state;
+
+    //舞台大小
+    let stageDOM = me.refs.stage,
+        stageW = stageDOM.scrollWidth,
+        stageH = stageDOM.scrollHeight,
+        halfStageW = Math.ceil(stageW / 2),
+        halfStageH = Math.ceil(stageH / 2);
+
+    //单个图片组件大小
+    let imgFigureDOM = me.refs.imgFigure0.figure, //TODO 获取的不是DOM元素
+        imgW = imgFigureDOM.scrollWidth,
+        imgH = imgFigureDOM.scrollHeight,
+        halfImgW = Math.ceil(imgW / 2),
+        halfImgH = Math.ceil(imgH / 2);
+
+    //计算中心图片的位置点
+    _.set(state, 'centerPos.left', halfStageW - halfImgW);
+    _.set(state, 'centerPos.top', halfStageH - halfImgH);
+
+    //计算左侧,右侧图片位置取值区间
+    _.set(state, 'hPosRange.leftSecX', [-halfImgW, halfStageW - halfImgW * 3]);
+    _.set(state, 'hPosRange.rightSecX', [halfStageW + halfImgW, stageW - halfImgW]);
+    _.set(state, 'hPosRange.y', [-halfImgH, halfStageH - halfImgH]);
+
+    //计算上侧图片位置取值区间
+    _.set(state, 'vPosRange.x', [halfStageW - halfImgW, halfStageW]);
+    _.set(state, 'vPosRange.topY', [-halfImgW, halfStageH - halfImgH * 3]);
+
+    me.hanlderLayoutPicture();
+  }
+
+  /*
+   * 重新布局图片
+   * @param centerIndex 制定居中图片
+  */
+  hanlderLayoutPicture (centerIndex) {
+    let me = this,
+        state =  me.state;
+
+    let imagesPos = _.get(state, 'imagesPos');
+    _.set(imagesPos, [centerIndex || 0], _.get(state, 'centerPos'));
+
   }
 }
 
