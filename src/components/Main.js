737e068018b21e529ea86c79828d08566f1e266f
surce2010 <345561171@qq.com>
2016-06-08 16:01:05 +0800

    TODO 角度旋转

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,6 +5,22 @@ require('styles/main.less');
 import React from 'react';
 import _ from 'lodash';
 
+let CONSTANT = {
+  centerPos: { //中间取值范围
+    left: 0,
+    top: 0
+  },
+  hPosRange: { //水平方向取值范围
+    leftSecX: [0, 0],
+    rightSecX: [0, 0],
+    y: [0, 0]
+  },
+  vPosRange: { //垂直方向取值范围
+    x: [0, 0],
+    topY: [0, 0]
+  }
+};
+
 // 获取图片相关的数
 var imagesData = require('../data/imagesData.json');
 // 利用自执行函数， 将图片名信息转成图片URL路径信息
@@ -37,19 +53,6 @@ class AppComponent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      centerPos: { //中间取值范围
-        left: 0,
-        top: 0
-      },
-      hPosRange: { //水平方向取值范围
-        leftSecX: [0, 0],
-        rightSecX: [0, 0],
-        y: [0, 0]
-      },
-      vPosRange: { //垂直方向取值范围
-        x: [0, 0],
-        topY: [0, 0]
-      },
       imagesPos: [] //图片数组位置
     };
   }
@@ -84,8 +87,7 @@ class AppComponent extends React.Component {
 
   //组件加载以后,为每一个图片计算位置取值区间
   componentDidMount() {
-    let me = this,
-        state = me.state;
+    let me = this;
 
     //舞台大小
     let stageDOM = me.refs.stage,
@@ -102,33 +104,69 @@ class AppComponent extends React.Component {
         halfImgH = Math.ceil(imgH / 2);
 
     //计算中心图片的位置点
-    _.set(state, 'centerPos.left', halfStageW - halfImgW);
-    _.set(state, 'centerPos.top', halfStageH - halfImgH);
+    _.set(CONSTANT, 'centerPos.left', halfStageW - halfImgW);
+    _.set(CONSTANT, 'centerPos.top', halfStageH - halfImgH);
 
     //计算左侧,右侧图片位置取值区间
-    _.set(state, 'hPosRange.leftSecX', [-halfImgW, halfStageW - halfImgW * 3]);
-    _.set(state, 'hPosRange.rightSecX', [halfStageW + halfImgW, stageW - halfImgW]);
-    _.set(state, 'hPosRange.y', [-halfImgH, halfStageH - halfImgH]);
+    _.set(CONSTANT, 'hPosRange.leftSecX', [-halfImgW, halfStageW - halfImgW * 3]);
+    _.set(CONSTANT, 'hPosRange.rightSecX', [halfStageW + halfImgW, stageW - halfImgW]);
+    _.set(CONSTANT, 'hPosRange.y', [-halfImgH, halfStageH - halfImgH]);
 
     //计算上侧图片位置取值区间
-    _.set(state, 'vPosRange.x', [halfStageW - halfImgW, halfStageW]);
-    _.set(state, 'vPosRange.topY', [-halfImgW, halfStageH - halfImgH * 3]);
+    _.set(CONSTANT, 'vPosRange.x', [halfStageW - halfImgW, halfStageW]);
+    _.set(CONSTANT, 'vPosRange.topY', [-halfImgH, halfStageH - halfImgH * 3]);
 
-    me.hanlderLayoutPicture();
+    me.hanlderLayoutPicture(5);
+  }
+
+  /*
+   * 计算随机数
+   * @param low, high取值区间的端点值
+   */
+  calcRandomNumber (low, high) {
+    return Math.floor(Math.random() * (high - low) + low);
   }
 
   /*
    * 重新布局图片
-   * @param centerIndex 制定居中图片
-  */
+   * @param centerIndex 居中图片索引
+   */
   hanlderLayoutPicture (centerIndex) {
     let me = this,
-        state =  me.state;
+        state = me.state,
+        imagesPos = _.get(state, 'imagesPos');
 
-    let imagesPos = _.get(state, 'imagesPos');
-    _.set(imagesPos, [centerIndex || 0], _.get(state, 'centerPos'));
+    //重绘居中图片
+    imagesPos.splice(centerIndex, 1);
+
+    //重绘上侧图片
+    if (imagesPos.length > 1) {
+      let topIndex = Math.floor(Math.random() * (imagesPos.length - 1));
+      imagesPos.splice(topIndex, 1);
+
+      //重绘左,右侧图片
+      if (imagesPos.length) {
+        let l = imagesPos.length;
+        for (let i = 0, j = Math.floor(l / 2); i < j, j < l; i++, j++) {
+          imagesPos[i] = {left: me.calcRandomNumber(_.get(CONSTANT, ['hPosRange', 'leftSecX', 0]), _.get(CONSTANT, ['hPosRange', 'leftSecX', 1])), top: me.calcRandomNumber(_.get(CONSTANT, ['hPosRange', 'y', 0]), _.get(CONSTANT, ['hPosRange', 'y', 1]))};
+          imagesPos[j] = {left: me.calcRandomNumber(_.get(CONSTANT, ['hPosRange', 'rightSecX', 0]), _.get(CONSTANT, ['hPosRange', 'rightSecX', 1])), top: me.calcRandomNumber(_.get(CONSTANT, ['hPosRange', 'y', 0]), _.get(CONSTANT, ['hPosRange', 'y', 1]))};
+        }
+      }
+
+      imagesPos.splice(topIndex, 0, {left: me.calcRandomNumber(_.get(CONSTANT, ['vPosRange', 'x', 0]), _.get(CONSTANT, ['vPosRange', 'x', 1])), top: me.calcRandomNumber(_.get(CONSTANT, ['vPosRange', 'topY', 0]), _.get(CONSTANT, ['vPosRange', 'topY', 1]))});
+    }
+
+    imagesPos.splice(centerIndex, 0, _.get(CONSTANT, 'centerPos'));
+
+    me.setState({
+      imagesPos: imagesPos
+    }, () => {
+      console.log(state, 'imagesPos');
+    });
 
   }
+
+
 }
 
 AppComponent.defaultProps = {
