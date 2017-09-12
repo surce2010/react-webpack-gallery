778a2a44101aa32f4e83ff9b15cd603fc98d7733
dingshaohua <345561171@qq.com>
2017-09-12 20:09:07 +0800

    pm2 deploy

--- cfg/defaults.js
+++ cfg/defaults.js
@@ -1,7 +1,7 @@
 'use strict';
 const path = require('path');
 const srcPath = path.join(__dirname, '/../src');
-const dfltPort = 8000;
+const dfltPort = 8002;
 function getDefaultModules() {
   return {
     preLoaders: [{
